a77a664e90ffae7ef7aea7530601f7c89e1ad6e5
Ritik <shrestharitik@gmail.com>
2024-06-10 10:10:26 +0545

    refactor boundary condition

--- js/ball.js
+++ js/ball.js
@@ -1,6 +1,4 @@
 class Ball {
-  speed = 0.7;
-
   constructor(x = 0, y = 0, r = 80, color = "red") {
     this.velocity = { x: getRandomValue(-2, 2), y: getRandomValue(-2, 2) };
     this.x = x;
@@ -22,24 +20,25 @@ class Ball {
 
   move = () => {
     // move x and y position
-    this.x += this.velocity.x * this.speed;
-    this.y += this.velocity.y * this.speed;
+    this.x += this.velocity.x * SPEED;
+    this.y += this.velocity.y * SPEED;
 
     this.element.style.transform = `translate(${this.x}px,${this.y}px)`;
   };
   detectCollision = () => {
     //clip the balls on the boundary
-    if (this.x + this.r > BOUNDARY_WIDTH) {
-      this.x = Math.max(this.r, this.x);
-      this.velocity.x *= -1;
-    } else if (this.x < 0) {
+    if (this.x + this.r >= BOUNDARY_WIDTH) {
       this.x = Math.min(BOUNDARY_WIDTH - this.r, this.x);
       this.velocity.x *= -1;
-    } else if (this.y + this.r > BOUNDARY_HEIGHT) {
-      this.y = Math.max(this.r, this.y);
+    } else if (this.x <= 0) {
+      this.x = Math.max(this.r, this.x);
+      this.velocity.x *= -1;
+    }
+    if (this.y + this.r >= BOUNDARY_HEIGHT) {
+      this.y = Math.min(BOUNDARY_HEIGHT - this.r, this.y);
       this.velocity.y *= -1;
-    } else if (this.y < 0) {
-      this.y = Math.min(this.y + BOUNDARY_HEIGHT - this.r, this.y);
+    } else if (this.y <= 0) {
+      this.y = Math.max(this.r, this.y);
       this.velocity.y *= -1;
     }
   };
